88e63a73f5b8514c1310a375ef6524345c8d1c5f
behnamrhp <behnamrahimpour74@gmail.com>
2024-01-03 00:26:47 +0300

    Add push method to doubly linked list

--- src/doubly-linked-list/doubly-linked-list.ts
+++ src/doubly-linked-list/doubly-linked-list.ts
@@ -24,6 +24,38 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val: unknown) {
+    // make new node for val
+    const newTail = new Node(val)
+    // check there is no head so put head and tail to new node
+    if (!this.head || !this.tail) {
+      this.head = newTail
+      this.tail = newTail
+      this.length++
+      return this
+    }
+    // if there is head get default tail
+    const defaultTail = this.tail
+    // set next for default tail to this new node
+    defaultTail.next = newTail
+    // set new node as new tail
+    this.tail = newTail 
+    // set prev of new node as default tail
+    newTail.prev = defaultTail
+    // increase length 
+    this.length++
+    // return class
+    return this
+  }
 }
+
+const list = new DoublyLinkedList()
+
+list.push("hello")
+list.push("world")
+list.push("to")
+list.push("behnam")
+console.log(list.head?.next)
 
 export {}
