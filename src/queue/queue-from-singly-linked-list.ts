d6ce4567bfc014a5a22f5b7ab0707b01490534a2
behnamrhp <behnamrahimpour74@gmail.com>
2024-01-22 22:30:19 +0300

    Add Queue

---
+++ src/queue/queue-from-singly-linked-list.ts
@@ -0,0 +1,58 @@
+class Node {
+  next: Node | null
+
+  value: unknown
+
+  constructor(value: unknown) {
+    this.value = value;
+    this.next = null
+  }
+}
+
+class Queue {
+  first: Node | null
+
+  last: Node | null
+
+  size = 0;
+
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  enqueue(value: unknown) {
+    const newQueue = new Node(value)
+    // if size is zero pass last and first and increase size
+    if (this.size === 0) {
+      this.first = newQueue;
+      this.last = newQueue;
+      return this.size++;
+    } 
+    // pass next of last to new Queue
+    (this.last as Node).next = newQueue
+    // set last as new queue
+    this.last = newQueue
+    // increase size
+    this.size++
+    return this.size++;
+  }
+
+  dequeue() {
+    // if there is no first return null
+    if (!this.first) return;
+    // save first in a variable
+    const temp = this.first
+    if (this.first === this.last) {
+      this.last = null
+    } 
+    // set next of first as first
+    this.first = this.first.next
+    // decrease size
+    this.size--
+    // return saved first
+    return temp.value
+  }
+}
+
+export {}
