eb3e93c100409d970a1b3cfe3b26e8d895a3ff6d
behnamrhp <behnamrahimpour74@gmail.com>
2024-01-30 11:06:54 +0300

    Add get and set method for hash map

--- src/hash-map/hash-map.ts
+++ src/hash-map/hash-map.ts
@@ -1,14 +1,14 @@
 export default class HashMap {
-  private keymap: Array<(string[])[]>
+  keymap: Array<(string[])[]>
   
-  constructor(size: number) {
-    this.keymap = new Array(size)
+  constructor(size?: number) {
+    this.keymap = new Array(size || 53)
   }
 
-  private hash(key: string) {
+  private hash(key: string): number {
     const ALetterCharCodeMinusOne = 96;
+    const WEIRD_PRIME = 31
     let total = 0
-    let WEIRD_PRIME = 31
 
     for (let i = 0; i < Math.min(key.length, 100); i++) {
       const char = key[i] as string;
@@ -18,7 +18,7 @@ export default class HashMap {
     return total
   }
 
-  private set(key: string, value: string) {
+  set(key: string, value: string) {
     // Get hashed key
     const index = this.hash(key)
     // Save value in an array
@@ -28,4 +28,27 @@ export default class HashMap {
     // Put array in key index of hashMap
     this.keymap[index]?.push([key, value])
   }
+
+  get(key: string) {
+    // Get hashed index
+    const index = this.hash(key)
+    // Get key, value from keymap by index
+    const keyValues = this.keymap[index]
+    if (!keyValues) return;
+    // find related key for our key
+    const keyValue = keyValues.find((item) => item[0] === key) || []
+    // get second value
+    const value = keyValue[1]
+    // return
+    return value
+  }
 }
+
+const hashMap = new HashMap()
+
+hashMap.set('white', '#fff')
+hashMap.set('black', '#000')
+
+
+console.log(hashMap.keymap)
+console.log('get method', hashMap.get('white'))
