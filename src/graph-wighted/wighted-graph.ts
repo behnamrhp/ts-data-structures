22b5427e077544ba7acb8f62339ea2edf76f0fd6
behnamrhp <behnamrahimpour74@gmail.com>
2024-02-10 20:11:03 +0300

    Add log

--- src/graph-wighted/wighted-graph.ts
+++ src/graph-wighted/wighted-graph.ts
@@ -103,7 +103,7 @@ graph.addEdge("C","F", 4);
 graph.addEdge("D","E", 3);
 graph.addEdge("D","F", 1);
 graph.addEdge("E","F", 1);
-// console.log(graph.adjacencyList)
+console.log(graph.adjacencyList)
 
 console.log(graph.dijkstra("A", "E"));
 graph.addEdge("C","F", 4);
